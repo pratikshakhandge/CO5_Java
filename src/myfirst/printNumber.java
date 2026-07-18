283e53bbaf99b669c4ac5c4108c6e1d55523e95c
pratikshakhandge <pratikshakhandge2006@gmail.com>
2026-07-18 23:58:19 +0530

    basic code done

---
+++ src/myfirst/printNumber.java
@@ -0,0 +1,17 @@
+package myfirst;
+
+public class printNumber {
+
+	public static void main(String args[]) {
+		int n=10;
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.println(i+" "+j);
+			}
+		}
+		
+	}
+}
